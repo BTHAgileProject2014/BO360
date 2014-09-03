8fc829a7ab87edd85404ec42bda07781cc90fd13
Pappskiva <allleo92@hotmail.com>
2014-09-03 16:28:30 +0200

    Google doc folder
    Länk till google doc folder

--- BO360/BO360/Main.h
+++ BO360/BO360/Main.h
@@ -9,4 +9,7 @@ private:
 	//I can edit this dohickie
 	//I can also edit, take that!
 	//Huehuheuhuehuehe
+	
+	//Google doc folder
+	//https://drive.google.com/folderview?id=0B4M02oQM-KwOTXE3V3MzUEFIYVk&usp=sharing
 };
