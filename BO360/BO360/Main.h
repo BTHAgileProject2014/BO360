17f86fce6885412b02d9250aee88ca5b2a57b57c
Jonas Axelsson <axelsson.92@live.se>
2014-09-03 16:47:04 +0200

    Removed test comments.

--- BO360/BO360/Main.h
+++ BO360/BO360/Main.h
@@ -3,11 +3,8 @@ class Main
 public:
 	Main();
 	~Main();
-	//Branch comments
+
 private:
 	void Run();
-	//I can edit this dohickie
-	//I can also edit, take that!
-	//Huehuheuhuehuehe
-	// <-- No space there? ):
+
 };
