875631f37a086bbbd041b2ddde8cd08a58b16eaf
Pappskiva <allleo92@hotmail.com>
2014-09-08 13:59:56 +0200

    remove head
    removed head

--- BO360/BO360/Main.h
+++ BO360/BO360/Main.h
@@ -6,14 +6,5 @@ public:
 
 private:
 	void Run();
-<<<<<<< HEAD
-	//I can edit this dohickie
-	//I can also edit, take that!
-	//Huehuheuhuehuehe
-	
-	//Google doc folder
-	//https://drive.google.com/folderview?id=0B4M02oQM-KwOTXE3V3MzUEFIYVk&usp=sharing
-=======
 
->>>>>>> origin/master
 };
