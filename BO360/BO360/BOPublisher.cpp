61d112ffd9919fbdd3a3c4f56059eaea47d98866
Johan Almeflo <johan_almeflo@hotmail.com>
2014-09-10 14:22:22 +0200

    Finilaze BOPublisher
    Added two notify for mouse movement and key presses + added struct
    needed for input in BOUtility

--- BO360/BO360/BOPublisher.cpp
+++ BO360/BO360/BOPublisher.cpp
@@ -10,10 +10,74 @@ BOPublisher::~BOPublisher()
 {
 }
 
+void BOPublisher::Initialize()
+{
+	m_subscribers = std::vector<BOSubscriber*>();
+
+	// All key messages is set to false
+	m_inputMessage = InputMessages();
+	m_inputMessage.mouseX = 0;
+	m_inputMessage.mouseY = 0;
+	m_inputMessage.leftMouseKey = false;
+	m_inputMessage.upArrow = false;;
+	m_inputMessage.downArrow = false;
+	m_inputMessage.leftArrow = false;
+	m_inputMessage.rightArrow = false;
+}
+
+void BOPublisher::Shutdown()
+{
+	for (auto element = m_subscribers.begin(); element != m_subscribers.end(); element++)
+	{
+		delete *element;
+	}
+	m_subscribers.clear();
+	
+}
+
 void BOPublisher::Notify()
 {
-	for (int i = 0; i < m_subscribers.size(); i++)
+	for (unsigned int i = 0; i < m_subscribers.size(); i++)
 	{
-		// m_subscribers->Notify(messages);
+		// m_subscribers->Notify(m_inputMessage);
 	}
+}
+
+// Change value for specified key (true = down, false = up)
+void BOPublisher::Notify(KeyMessages p_keyMessage, bool p_value)
+{
+	switch (p_keyMessage)
+	{
+	case KeyMessages::leftMouseKey:
+		m_inputMessage.leftMouseKey = p_value;
+		break;
+	case KeyMessages::upArrow:
+		m_inputMessage.upArrow = p_value;
+		break;
+	case KeyMessages::downArrow:
+		m_inputMessage.downArrow = p_value;
+		break;
+	case KeyMessages::leftArrow:
+		m_inputMessage.leftArrow = p_value;
+		break;
+	case KeyMessages::rightArrow:
+		m_inputMessage.rightArrow = p_value;
+		break;
+	}
+
+	Notify();
+}
+
+// Change position of the mouse pointer
+void BOPublisher::Notify(int p_x, int p_y)
+{
+	m_inputMessage.mouseX = p_x;
+	m_inputMessage.mouseY = p_y;
+
+	Notify();
+}
+
+void BOPublisher::AddSubscriber(BOSubscriber* p_subscriber)
+{
+	m_subscribers.push_back(p_subscriber);
 }
--- BO360/BO360/BOPublisher.h
+++ BO360/BO360/BOPublisher.h
@@ -1,30 +1,23 @@
 #ifndef BOPUBLISHER_H_
 #define BOPUBLISHER_H_
 #include "BOSubscriber.h"
+#include "BOUtility.h"
 #include <vector>
 class BOPublisher
 {
 private:
-
-	struct KeyMessages
-	{
-		int mouseX;
-		int mouseY;
-		bool mouseKeyDown;
-		bool rightArrow;
-		bool leftArrow;
-		bool upArrow;
-		bool downArrow;
-	};
-
-
 	std::vector<BOSubscriber*> m_subscribers;
-	KeyMessages m_keyMessage;
+	InputMessages m_inputMessage;
+	void Notify();
 public:
 	BOPublisher();
 	~BOPublisher();
-	void Notify();
-	//void Notify(KeyMessages p_keyMessages);
 
+	void Initialize();
+	void Shutdown();
+
+	void Notify(KeyMessages p_keyMessage, bool p_value);
+	void Notify(int p_x, int p_y);
+	void AddSubscriber(BOSubscriber* p_subscriber);
 };
 #endif
