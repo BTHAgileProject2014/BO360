eb58fe4fcc138a43df1e9189a1284924e81acd87
Viktor <Phelsuma_66@hotmail.com>
2014-09-10 18:10:13 +0200

    added ball movement in update and float2 calc

--- BO360/BO360/BOBall.cpp
+++ BO360/BO360/BOBall.cpp
@@ -11,5 +11,26 @@ BOBall::~BOBall()
 
 void BOBall::Update()
 {
-	// TODO: Add ball mechanics.
+	m_position.x = m_speed * m_direction.x + m_position.x;
+	m_position.y = m_speed * m_direction.y + m_position.y;
+}
+
+void BOBall::SetSpeed(float p_speed)
+{
+	m_speed = p_speed;
+}
+
+float BOBall::GetSpeed()
+{
+	return m_speed;
+}
+
+void BOBall::SetDirection(float2 p_direction)
+{
+	m_direction = p_direction.normalized();
+}
+
+float2 BOBall::GetDirection()
+{
+	return m_direction;
 }
--- BO360/BO360/BOBall.h
+++ BO360/BO360/BOBall.h
@@ -10,5 +10,14 @@ public:
 	~BOBall();
 
 	void Update();
+	void SetSpeed(float p_speed);
+	float GetSpeed();
+	void SetDirection(float2 p_direction);
+	float2 GetDirection();
+
+private:
+	float m_speed;
+	float2 m_direction;
+
 };
 #endif
