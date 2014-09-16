5e6f58f28b15f7f19857cd61a133b05ab1b30b66
Johan Almeflo <johan_almeflo@hotmail.com>
2014-09-16 16:20:57 +0200

    Init of power ups

---
+++ BO360/BO360/BOPowerUp.cpp
@@ -0,0 +1,28 @@
+#include "BOPowerUp.h"
+
+
+BOPowerUp::BOPowerUp()
+{
+}
+
+
+BOPowerUp::~BOPowerUp()
+{
+}
+
+bool BOPowerUp::Initialize(float2 p_position, int2 p_size, std::string p_fileName)
+{
+	return BOObject::Initialize(p_position, p_size, p_fileName);
+}
+
+void BOPowerUp::Shutdown()
+{
+}
+
+void BOPowerUp::Draw()
+{
+}
+
+void BOPowerUp::Update()
+{
+}
