473b5010311949b1ed419144f2d7b82b07840549
Mattias Willemsen <thehippiecoder@gmail.com>
2014-09-16 14:27:36 +0200

    Added line ending to output

--- BO360/BO360/BOGraphicInterface.cpp
+++ BO360/BO360/BOGraphicInterface.cpp
@@ -136,7 +136,7 @@ SDL_Texture* BOGraphicInterface::LoadTexture(std::string p_path)
 	if (surface == NULL)
 	{
 		std::cout << "Failed to load image: " + p_path << std::endl;
-		std::cout << IMG_GetError();
+		std::cout << IMG_GetError() << std::endl;
 		// Failed to load! 
 		// This should be handled
 	}
