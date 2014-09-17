6eed60314c9a51729c39431acfbb88ff4b48e2c8
Pappskiva <allleo92@hotmail.com>
2014-09-17 09:17:13 +0200

    MOre basic shield stuffs

--- BO360/BO360/BOShield.cpp
+++ BO360/BO360/BOShield.cpp
@@ -4,13 +4,36 @@
 BOShield::BOShield()
 {
 	m_IsActive = false;
+	m_ShieldSprite = NULL;
 }
 
 
-BOShield::~BOShield()
+BOShield::~BOShield(){}
+
+bool BOShield::Initialize(float2 p_ShieldPos, int2 p_ShieldSize, std::string p_ShieldFN,
+	float2 p_POPos, int2 p_POSize, std::string p_POFN)
 {
-}
+	m_ShieldPosition = p_ShieldPos;
+	m_ShieldSize = p_ShieldSize;
 
+	m_ShieldSprite = BOGraphicInterface::LoadTexture(p_ShieldFN);
+	BOPowerUp::Initialize(p_POPos, p_POSize, p_POFN);
+
+	return true;
+}
+void BOShield::Update()
+{
+	
+
+	BOPowerUp::Update();
+}
+void BOShield::Draw()
+{
+	if (m_IsActive)
+	{
+		BOGraphicInterface::Draw(m_ShieldSprite, m_ShieldPosition, m_ShieldSize);
+	}
+}
 void BOShield::SetActive(bool p_IsActive)
 {
 	m_IsActive = p_IsActive;
--- BO360/BO360/BOShield.h
+++ BO360/BO360/BOShield.h
@@ -1,18 +1,32 @@
 #ifndef BOSHIELD_H_
 #define BOSHIELD_H_
 #include "BOPowerUp.h"
+#include "BOPhysics.h"
+
 class BOShield : BOPowerUp
 {
 public:
 	BOShield();
 	~BOShield();
 
+	bool Initialize(float2 p_ShieldPos, int2 p_ShieldSize, std::string p_ShieldFN,
+				float2 p_POPos, int2 p_POSize, std::string p_POFN);
+	void Update();
+	void Draw();
+
 	void SetActive(bool p_IsActive);
 	void SwitchActive();
 	bool GetActive();
 
+	
+
+
 private:
 	bool m_IsActive;
+	SDL_Texture* m_ShieldSprite;
+	int2 m_ShieldSize;
+	float2 m_ShieldPosition;
+
 };
 
 #endif
