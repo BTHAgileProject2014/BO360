edc499cb6b91f52f87ce1f9f0b85e13fe6139ecd
Johan Almeflo <johan_almeflo@hotmail.com>
2014-09-17 09:03:23 +0200

    PowerUps Update + Activate
    The base class move the power up to the center if the bool m_hasSpawned
    is set to true

--- BO360/BO360/BOPowerUp.cpp
+++ BO360/BO360/BOPowerUp.cpp
@@ -10,8 +10,17 @@ BOPowerUp::~BOPowerUp()
 {
 }
 
-bool BOPowerUp::Initialize(float2 p_position, int2 p_size, std::string p_fileName)
+bool BOPowerUp::Initialize(float2 p_position, int2 p_size, std::string p_fileName, float p_speed, int2 p_windowSize)
 {
+	m_hasSpawned = true;
+	m_speed = p_speed;
+	m_windowSize = p_windowSize;
+
+	// Set direction to center of screen
+	m_direction.x = (p_windowSize.x / 2.0f) - m_position.x;
+	m_direction.y = (p_windowSize.y / 2.0f) - m_position.y;
+	m_direction.normalized();
+
 	return BOObject::Initialize(p_position, p_size, p_fileName);
 }
 
@@ -19,10 +28,15 @@ void BOPowerUp::Shutdown()
 {
 }
 
-void BOPowerUp::Draw()
+void BOPowerUp::Update()
 {
+	if (m_hasSpawned)
+	{
+		m_position.x = m_speed * m_direction.x + m_position.x;
+		m_position.y = m_speed * m_direction.y + m_position.y;
+	}
 }
 
-void BOPowerUp::Update()
+void BOPowerUp::Activate()
 {
 }
