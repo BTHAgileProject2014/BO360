2c7857c37803dcde64113275376744aa00937afe
Pappskiva <allleo92@hotmail.com>
2014-09-22 13:21:56 +0200

    Rocket ball
    Bollen "skjuts" iväg då den nuddar padden.

--- BO360/BO360/BOBall.cpp
+++ BO360/BO360/BOBall.cpp
@@ -33,16 +33,23 @@ void BOBall::Update(Uint32 p_deltaTime)
 	{
 		return;
 	}
+	if (m_Fuel > 0)
+	{
+		m_position.x = (m_speed * p_deltaTime) * m_direction.x + m_position.x;
+		m_position.y = (m_speed * p_deltaTime) * m_direction.y + m_position.y;
+	}
+	else
+	{
+		m_position.x = (0.75*m_speed * p_deltaTime) * m_direction.x + m_position.x;
+		m_position.y = (0.75*m_speed * p_deltaTime) * m_direction.y + m_position.y;
+	}
 
-	m_position.x = (m_speed * p_deltaTime) * m_direction.x + m_position.x;
-	m_position.y = (m_speed * p_deltaTime) * m_direction.y + m_position.y;
-
-	if (m_position.x < (m_size.x / 2) || m_position.x > (m_windowSize.x - (m_size.x / 2)))
+	if (m_position.x < (m_size.x / 2) || m_position.x >(m_windowSize.x - (m_size.x / 2)))
 	{
 		m_canColide = true;
 		m_direction.x *= -1;
 	}
-	if (m_position.y < (m_size.y / 2) || m_position.y > (m_windowSize.y - (m_size.y / 2)))
+	if (m_position.y < (m_size.y / 2) || m_position.y >(m_windowSize.y - (m_size.y / 2)))
 	{
 		m_canColide = true;
 		m_direction.y *= -1;
