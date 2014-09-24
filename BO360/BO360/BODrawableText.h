e37da79459106b918acddfe85b6ee63ce37ee36a
forslin <zyrrewow@hotmail.com>
2014-09-24 13:44:57 +0200

    Fix

--- BO360/BO360/BODrawableText.h
+++ BO360/BO360/BODrawableText.h
@@ -19,14 +19,12 @@ public:
 	void SetText(std::string p_text, int3 p_color, int p_maxWidth);
 	float2 GetPosition();
 	int2 GetSize();
-	void EnableText(bool p_enable);
 
 private:
 	std::string m_text;
 	SDL_Texture* m_texture;
 	int2 m_size;
 	float2 m_position;
-	bool m_enabled;
 	int m_fontSize;
 };
 #endif
