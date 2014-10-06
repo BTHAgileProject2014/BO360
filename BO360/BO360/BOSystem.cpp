1286fda737c09132c53b1fe4fdceeeb3e06bf9d4
Viktor <Phelsuma_66@hotmail.com>
2014-10-06 16:47:57 +0200

    Neon theme

--- BO360/BO360/BOSystem.cpp
+++ BO360/BO360/BOSystem.cpp
@@ -50,7 +50,7 @@ bool BOSystem::Initialize()
 	}
 
 	// Initialize the texturemanager
-	if (!BOTextureManager::Initialize("Industrial"))
+	if (!BOTextureManager::Initialize("Neon"))
 	{
 		std::cout << "Initialize texturemanager failed!" << std::endl;
 
