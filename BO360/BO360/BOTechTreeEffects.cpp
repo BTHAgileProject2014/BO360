1194be8530a7377932883b916af8c1889ca11193
forslin <zyrrewow@hotmail.com>
2014-10-08 10:27:59 +0200

    TechTreeEffects

--- BO360/BO360/BOTechTreeEffects.cpp
+++ BO360/BO360/BOTechTreeEffects.cpp
@@ -9,3 +9,43 @@ BOTechTreeEffects::BOTechTreeEffects()
 BOTechTreeEffects::~BOTechTreeEffects()
 {
 }
+
+bool BOTechTreeEffects::Initialize()
+{
+    m_ballEffects = TechTreeBallEffects();
+    m_levelEffects = TechTreeLevelEffects();
+    m_paddleEffects = TechTreePaddleEffects();
+    m_PUEffects = TechTreePUEffects();
+    m_utilityEffects = TechTreeUtilityEffects();
+    return true;
+}
+
+void BOTechTreeEffects::Shutdown()
+{
+
+}
+
+TechTreeBallEffects BOTechTreeEffects::GetBallEffects()const
+{
+    return m_ballEffects;
+}
+
+TechTreeLevelEffects BOTechTreeEffects::GetLevelEffects()const
+{
+    return m_levelEffects;
+}
+
+TechTreePaddleEffects BOTechTreeEffects::GetPaddleEffects()const
+{
+    return m_paddleEffects;
+}
+
+TechTreePUEffects BOTechTreeEffects::GetPUEffects()const
+{
+    return m_PUEffects;
+}
+
+TechTreeUtilityEffects BOTechTreeEffects::GetUtilityEffects()const
+{
+    return m_utilityEffects;
+}
