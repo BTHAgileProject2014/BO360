4673eba7234b5868746fc05f2f4c8d9f9e8d95ba
Jonas Axelsson <axelsson.92@live.se>
2014-10-14 16:43:43 +0200

    Created every texture.

--- BO360/BO360/BOObjectManager.cpp
+++ BO360/BO360/BOObjectManager.cpp
@@ -334,7 +334,7 @@ void BOObjectManager::Handle(InputMessages p_inputMessage)
 }
 }
 
-    if (p_inputMessage.fKey && m_shockwave.Activate())
+    if (p_inputMessage.fKey)
     {
         ActivateShockwave();
     }
